92349226b855f44f0447b3c7073464bfdebc36c6
baojingh@163.com <baojingh@163.com>
2023-04-09 22:03:22 +0800

    ss

--- src/hellomodule/hellomodule.go
+++ src/hellomodule/hellomodule.go
@@ -1,7 +1,12 @@
 package main
 
-import "github.com/sirupsen/logrus"
+import (
+	"fmt"
+
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
 	logrus.Println("hello, gopath mode")
+	fmt.Println("hello, ncv9uioasnio")
 }
