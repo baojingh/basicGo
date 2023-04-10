098eee171af169f4ff9bdf6c3c7a6b4e9d6832b4
z003nujf <baojing.he@siemens.com>
2023-04-10 17:53:47 +0800

    Revert "type"
    This reverts commit a5fb995154e132f74d0d9f8db5130a238b5a0a28.

--- src/hellomodule/hellomodule.go
+++ src/hellomodule/hellomodule.go
@@ -2,29 +2,11 @@ package main
 
 import (
 	"fmt"
-	"os"
 
-	_ "github.com/go-redis/redis/v8"
-	"github.com/google/uuid"
 	"github.com/sirupsen/logrus"
 )
 
-func init() {
-	fmt.Println("this is init3...")
-}
-
-func init() {
-	fmt.Println("this is init1...")
-}
-
-func init() {
-	fmt.Println("this is init2...")
-}
-
 func main() {
 	logrus.Println("hello, gopath mode")
-	logrus.Println(uuid.NewString())
 	fmt.Println("hello, ncv9uioasnio")
-
-	fmt.Println("cavca", os.Getenv("GOPATH"))
 }
