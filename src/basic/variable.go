272940efc67e7bbf7e4f912ec220207908c49571
z003nujf <baojing.he@siemens.com>
2023-04-11 17:21:40 +0800

    map

--- src/basic/variable.go
+++ src/basic/variable.go
@@ -2,19 +2,18 @@ package main
 
 import (
 	"fmt"
-	"unsafe"
 )
 
-func foo(arr [5]int8) {
-	fmt.Printf("%d\n", arr[0])
-	fmt.Printf("%d\n", len(arr))
-	fmt.Printf("%d\n", unsafe.Sizeof(arr))
-}
+// func foo(arr [5]int8) {
+// 	fmt.Printf("%d\n", arr[0])
+// 	fmt.Printf("%d\n", len(arr))
+// 	fmt.Printf("%d\n", unsafe.Sizeof(arr))
+// }
 
 func main() {
 
-	var arr1 = [5]int8{1, 2, 3, 4, 5}
-	foo(arr1)
+	m1 := make(map[string]int, 8)
+	m1["k1"] = 1
 
 	fmt.Println("this is end")
 }
