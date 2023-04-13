52c0c7e5fcf28bd2e7574ddc4223196e20b0156e
z003nujf <baojingh@163.com>
2023-04-13 17:55:38 +0800

    gin

--- web-server/router/router.go
+++ web-server/router/router.go
@@ -6,10 +6,12 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func regHello(context *gin.Context) {
+	context.String(http.StatusOK, "hello")
+}
+
 func SetupRouter() *gin.Engine {
 	router := gin.Default()
-	router.GET("/", func(contenxt *gin.Context) {
-		contenxt.String(http.StatusOK, "hello")
-	})
+	router.GET("/", regHello)
 	return router
 }
