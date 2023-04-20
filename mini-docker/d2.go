27d8a377c0c83bd584e5712bc4dd83a773c03714
z003nujf <baojingh@163.com>
2023-04-20 18:01:57 +0800

    mem

--- mini-docker/d2.go
+++ mini-docker/d2.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path"
+	"strconv"
 	"syscall"
 
 	log "github.com/sirupsen/logrus"
 )
 
-func run() {
-	cmd := exec.Command(os.Args[2])
+const cgroupMemoryHierarchyMout = "/sys/fs/cgroup/memory"
 
+func main() {
+	arg := os.Args
+	if arg[0] == "/proc/self/exe" {
+		log.Println("current pid is ", syscall.Getpid())
+		log.Println()
+		// cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
+		cmd := exec.Command("sh", "-c", `df -h`)
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			log.Println(err)
+			os.Exit(1)
+		}
+
+	}
+	cmd := exec.Command("/proc/self/exe")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS |
 			syscall.CLONE_NEWIPC |
@@ -20,21 +40,21 @@ func run() {
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	err := cmd.Run()
-	if err != nil {
-		log.Println("failed to exec the command ", err)
-	}
-}
+	if err := cmd.Start(); err != nil {
+		log.Println("ERROR", err)
+		os.Exit(1)
+	} else {
+		log.Println("%v", cmd.Process.Pid)
+		os.Mkdir(path.Join(cgroupMemoryHierarchyMout, "testmemorylimit"), 0755)
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMout, "testmemorylimit", "tasks"),
+			[]byte(strconv.Itoa(cmd.Process.Pid)), 0644)
+		ioutil.WriteFile(path.Join(cgroupMemoryHierarchyMout, "testmemorylimit",
+			"memory.limit in bytes"), []byte("lOOm"), 0644)
 
-func main() {
-	arg := os.Args
-
-	switch arg[1] {
-	case "run":
-		run()
-	default:
-		panic("the arg is not defined")
 	}
+
+	cmd.Process.Wait()
+
 	log.Println("################")
 
 }
