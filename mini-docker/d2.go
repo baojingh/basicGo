efad7c6d201d60fce7020e59a0a629d980a24107
z003nujf <baojingh@163.com>
2023-04-20 16:14:18 +0800

    demo

--- mini-docker/d2.go
+++ mini-docker/d2.go
@@ -1,25 +1,28 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"os/exec"
 	"syscall"
+
+	log "github.com/sirupsen/logrus"
 )
 
 func run() {
 	cmd := exec.Command(os.Args[2])
 
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS |
+			syscall.CLONE_NEWIPC |
+			syscall.CLONE_NEWNET |
+			syscall.CLONE_NEWPID,
 	}
-
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
-		fmt.Println("failed to exec the command ", err)
+		log.Println("failed to exec the command ", err)
 	}
 }
 
@@ -32,7 +35,6 @@ func main() {
 	default:
 		panic("the arg is not defined")
 	}
-
-	fmt.Println("##############")
+	log.Println("################")
 
 }
