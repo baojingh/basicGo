8fff98e3c86f9f00df1a9c441ef9e15cc960fbec
z003nujf <baojingh@163.com>
2023-05-31 09:00:17 +0800

    update: arr

--- basic/dockerGramar/mapArrayDemo.go
+++ basic/dockerGramar/mapArrayDemo.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 /**
   @Author   : bob
   @Datetime : 2023-05-29 下午 10:36
@@ -13,11 +15,35 @@ package main
 // are case-sensitive.
 type Values map[string][]string
 
-Get()
+func (v Values) Set(key, value string) {
+	v[key] = []string{value}
+}
+
+func (v Values) Add(key, value string) {
+	v[key] = append(v[key], value)
+}
+
+// Get get the first value
+func (v Values) Get(key string) string {
+	if v == nil {
+		return ""
+	}
+	val := v[key]
+	if len(val) == 0 {
+		return ""
+	}
+	return val[0]
+}
 
 func main() {
 	mapArr := Values{}
-	mapArr.Set("sydout", "1")
+	mapArr.Set("stdout", "v1")
+	mapArr.Set("stderr", "v1")
 
+	//mapArr.Set("stdout", "v2")
+	//mapArr.Set("stderr", "v2")
 
+	for k, vArr := range mapArr {
+		fmt.Printf("%s, %s\n", k, vArr)
+	}
 }
