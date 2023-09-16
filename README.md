705130f988ffccb8da2a95cecf492a2abfa32b46
baojingh@163.com <baojingh@163.com>
2023-09-16 17:04:22 +0800

    update: log

--- README.md
+++ README.md
@@ -4,6 +4,13 @@
 https://github.com/link1st/gowebsocket
 
 
+### 创建项目
+项目名称是backend-go
+在项目的根目录下，执行
+```shell
+go mod init backend-go
+```
+
 
 ### 新增依赖
 1. 将需要的依赖添加到import中
--- basic/logdemo/logger/logger.go
+++ basic/logdemo/logger/logger.go
@@ -9,41 +9,42 @@ package logger
 import (
 	"fmt"
 	"io"
+	"os"
 	"path/filepath"
 	"time"
 
 	rotate "github.com/lestrrat-go/file-rotatelogs"
-	logger "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
 
-var log = logger.New()
+var log = logrus.New()
 
 // init init logs
 func init() {
-	path := "./"
+	path := "/var/log/mdocker/"
 
 	writer, err := rotate.New(
-		filepath.Join(path, fmt.Sprintf("test-%s.log", "%Y%m%d")),
-		rotate.WithLinkName(filepath.Join(path, "test.log")),
-		rotate.WithRotationCount(7),
+		filepath.Join(path, fmt.Sprintf("mdocker-%s.log", "%Y%m%d")),
+		rotate.WithLinkName(filepath.Join(path, "mdocker.log")),
+		rotate.WithRotationCount(5),
 		rotate.WithRotationTime(time.Hour*24),
 	)
 	if err == nil {
-		log.SetOutput(io.MultiWriter(writer /*, os.Stdout*/))
+		log.SetOutput(io.MultiWriter(writer, os.Stdout))
+	} else {
+		log.Fatal("Config logger failure, ", err)
 	}
+
+	log.SetFormatter(&logrus.TextFormatter{
+		TimestampFormat:           "2006-01-02 15:04:05",
+		ForceColors:               true,
+		EnvironmentOverrideColors: true,
+		FullTimestamp:             true,
+		DisableLevelTruncation:    true,
+	})
+	log.SetLevel(logrus.InfoLevel)
 }
 
-// New new Logger
-func New() *logger.Logger {
+func New() *logrus.Logger {
 	return log
-}
-
-// NewRotatedLogger new Logger
-func NewRotatedLogger(path string, t int, a int) (io.WriteCloser, error) {
-	return rotate.New(
-		fmt.Sprintf("%s.%s", path, "%Y%m%d%H%M"),
-		rotate.WithLinkName(path),
-		rotate.WithMaxAge(time.Minute*10),
-		rotate.WithRotationTime(time.Minute),
-	)
 }
