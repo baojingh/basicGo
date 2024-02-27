a04fbd367e742f78c84ddf0d1b3c3bcb9befbef7
baojingh <baojingh@163.com>
2024-02-27 14:40:25 +0800

    update

--- http-server/main.go
+++ http-server/main.go
@@ -8,7 +8,9 @@ import (
 
 func main() {
 	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/ied", ied)
+	http.HandleFunc("/all", all)
+	http.HandleFunc("/docker", docker)
+	http.HandleFunc("/sys_mem", sys_mem)
 	err := http.ListenAndServe(":9900", nil)
 	if err != nil {
 		fmt.Println("http server startup failure, ", err)
@@ -20,7 +22,7 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "hello from http server!\n")
 }
 
-func ied(w http.ResponseWriter, r *http.Request) {
+func all(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		body, err := ioutil.ReadAll(r.Body)
 		if err != nil {
@@ -32,3 +34,30 @@ func ied(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
 	}
 }
+
+func docker(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body",
+				http.StatusInternalServerError)
+		}
+		fmt.Println(string(body))
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+
+func sys_mem(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Error reading request body",
+				http.StatusInternalServerError)
+		}
+		fmt.Println(string(body))
+	} else {
+		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
+	}
+}
+
